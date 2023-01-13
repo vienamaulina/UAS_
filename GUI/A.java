0a597106855920042709aa57e78833b73ab29702
Viena Maulina <vienamaulina19@gmail.com>
2023-01-13 16:59:54 +0700

    file java

---
+++ GUI/A.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package a;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ASUS
+ */
+public class A {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame jFrame;
+
+        jFrame = new JFrame();
+        jFrame.setSize(400, 300);
+        jFrame.setVisible(true);
+
+        JOptionPane.showMessageDialog(jFrame, "how are you?");
+        JOptionPane.showMessageDialog(null, "good Bye");
+        // TODO code application logic here
+    }
+    
+}
